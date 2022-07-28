124c9e68babcfc4a5cbbfa37ab08c5cb1f50bb2e
anx450z <anx450z@gmail.com>
2022-07-28 23:26:36 +0530

    Made changes in postgres
    - changed pg_hba.conf file method to `trust`
    - created a database typeorm
    - minor changes to index =.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello!!!")
+import { DataSource } from "typeorm";
+
+const main = async () => {
+  try {
+    const dataSource = new DataSource({
+      type: "postgres",
+      host: "127.0.0.1",
+      port: 5432,
+      username: "postgres",
+      password: "ankur",
+      database: "typeorm",
+    });
+
+    let connection = await dataSource.initialize();
+    console.log("connected successfully to Postgres 😊")
+  } catch (error) {
+    console.log("🤔 something went wrong")
+    console.log(error);
+  }
+
+
+  // connection.manager./* your stuff...*/
+};
+
+main();
